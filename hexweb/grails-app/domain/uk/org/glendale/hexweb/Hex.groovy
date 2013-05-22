5506e4bcf8ac1df3be7b882ca5676332431c05c2
Samuel Penn <sam@glendale.org.uk>
2013-05-22 23:17:14 +0100

    Added missing comments.

--- hexweb/grails-app/domain/uk/org/glendale/hexweb/Hex.groovy
+++ hexweb/grails-app/domain/uk/org/glendale/hexweb/Hex.groovy
@@ -1,5 +1,18 @@
+/*
+ * Copyright (C) 2013 Samuel Penn, sam@glendale.org.uk
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation version 3, or
+ * any later version. See the file COPYING.
+ */
 package uk.org.glendale.hexweb
 
+/**
+ * A Hex is an individual tile on the map. Each hex has an x,y coordinate,
+ * and there can only ever be zero or one hex with a particular coordinate
+ * on a map.
+ */
 class Hex {
 	MapInfo		mapInfo
 	int			x
