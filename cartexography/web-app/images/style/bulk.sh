7e6147f76f468ddb17cb8c94bb534220d0bdec33
Samuel Penn <sam@glendale.org.uk>
2014-11-01 17:14:45 +0000

    Updated bulk.sh to handle sub directories in tile images.

--- cartexography/web-app/images/style/bulk.sh
+++ cartexography/web-app/images/style/bulk.sh
@@ -9,6 +9,9 @@ fi
 cd $1.xcf
 for d in *
 do
+
+if [ -d $d ]
+then
 cd $d
 
 gimp -n -i -b - <<EOF
@@ -24,7 +27,35 @@ gimp -n -i -b - <<EOF
   (gimp-quit 0)
   )
 EOF
+
+for s in *
+do
+if [ -d $s ]
+then
+cd $s
+if [ "*.xcf" != "" ]
+then
+gimp -n -i -b - <<EOF
+(let* ( (file's (cadr (file-glob "*.xcf" 1))) (filename "") (image 0) (layer 0) )
+  (while (pair? file's)
+    (set! image (car (gimp-file-load RUN-NONINTERACTIVE (car file's) (car file's))))
+    (set! layer (car (gimp-image-merge-visible-layers image CLIP-TO-IMAGE)))
+    (set! filename (string-append (substring (car file's) 0 (- (string-length (car file's)) 4)) ".png"))
+    (gimp-file-save RUN-NONINTERACTIVE image layer filename filename)
+    (gimp-image-delete image)
+    (set! file's (cdr file's))
+    )
+  (gimp-quit 0)
+  )
+EOF
+fi
+cd ..
+fi
+done
+
 mkdir -p ../../$1/$d
 mv *.png ../../$1/$d
+mv */*.png ../../$1/$d
 cd ..
+fi
 done
